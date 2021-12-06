44df2d00d7c2587620011aa54a589b05b62bb052
rbpatt2019 <rb.patterson.cross@gmail.com>
2021-12-06 17:14:40 +0000

    fix(move_coordinates.bash): use tmpfiles correctly
    The previous implementaion did not correctly iterate over the file. Each iteration acted on a clean input, so changes were not cumulative. To address this, an initial (temporary) copy of the GTF is created. For each loop, a tmp file is used to hold the output of awk, then moved back to the copied GTF. Finally, the copied gTF is moved to OUT, ensuring there is no waste.
    
    This is a prime location for speed ups. This is the best pure bash implementation I could sort out.

--- workflow/scripts/move_coordinates.bash
+++ workflow/scripts/move_coordinates.bash
@@ -15,19 +15,29 @@ while getopts ":b:g:o:" opt; do
   esac
 done
 
+# We want to iteratively modify while preserving the original file
+# So we create a tmpfile, modify that, then move it to output
+TMPGTF=$(mktemp)
+cp $GTF $TMPGTF
+
 # Read Bedfile, splitting on expected fields
 while read -r CHR NEWSTART NEWEND NAME; do
     # Extract current end from GTF
-    OLDEND=$(awk -v name="$NAME" '$3 == "gene" && $0 ~ name {print $5}' "$GTF")
+    OLDEND=$(awk -v name="$NAME" '$3 == "gene" && $0 ~ name {print $5}' "$TMPGTF")
     # Increment NEWEND as GTF is 1-indexed, while BED is 0-indexed
     ((NEWEND+=1))
     # For anyline that contains `name`
     # If field 5 (feature end) is OLDEND,
     # Replace with NEWWEND
+    # Also, we don't want to use sponge, so old fashioned tmp files
+    TMPFILE=$(mktemp)
     awk \
       -v oldend="$OLDEND" \
       -v newend="$NEWEND" \
       -v name="$NAME" \
       'BEGIN{FS=OFS="\t"} $5 == oldend && $0 ~ name {$5 = newend} 1' \
-      "$GTF" > "$OUT"
+      "$TMPGTF" > "$TMPFILE" &&
+    mv "$TMPFILE" "$TMPGTF"
 done < "$BEDFILE"
+
+mv "$TMPGTF" "$OUT"
